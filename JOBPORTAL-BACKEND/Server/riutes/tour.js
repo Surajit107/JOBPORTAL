5751240a8576c43a29c9538c84e5b7bff2f31cb6
Surajit Pal <palsurajit06@gmail.cpm>
2022-11-24 22:46:39 +0530

    BackEnd Removed

--- JOBPORTAL-BACKEND/Server/riutes/tour.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-import {createTour,getTours} from "../controller/tour.js";
-
-router.post("/", createTour);
-router.get("/", getTours);
-
-
-export default router;
