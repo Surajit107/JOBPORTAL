373b2a011b418f231e197eeb637674ce985d0079
Surajit Pal <palsurajit06@gmail.cpm>
2022-11-25 00:05:51 +0530

    warning removed

--- JOBPORTAL-FRONTEND/src/pages/SignIn.jsx
+++ JOBPORTAL-FRONTEND/src/pages/SignIn.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { Link, useNavigate } from 'react-router-dom'
 import Banner from '../components/common/banners/Banner'
 import { fetchSignIn } from '../redux/slice/AuthSlice'
